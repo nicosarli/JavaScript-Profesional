95d860a5203f215d83a5ce11be0027409fd65b3e
nicosarli <nico_sarli@hotmail.com>
2021-07-23 10:51:06 -0300

    Pasando los archivos a TypeScript

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -1,40 +1,34 @@
 class MediaPlayer {
+  media: HTMLMediaElement;
+  plugins: Array<any>;
   constructor(config) {
     this.media = config.el;
     this.plugins = config.plugins || [];
-    this._initPlugins();
+    this.initPlugins();
   }
-  _initPlugins() {
-    const player = {
-      play: () => this.play(),
-      pause: () => this.pause(),
-      media: this.media,
 
-      get muted() {
-        return this.media.muted;
-      },
-
-      set muted(value) {
-        this.media.muted = value;
-      },
-    };
-
+  private initPlugins() {
     this.plugins.forEach((plugin) => {
-      plugin.run(player);
+      plugin.run(this);
     });
   }
+
   play() {
     this.media.play();
   }
+
   pause() {
     this.media.pause();
   }
+
   togglePlay() {
     this.media.paused ? this.media.play() : this.media.pause();
   }
+
   mute() {
     this.media.muted = true;
   }
+
   unmute() {
     this.media.muted = false;
   }
--- assets/index.ts
+++ assets/index.ts
@@ -1,6 +1,6 @@
-import MediaPlayer from "./MediaPlayer.js";
-import AutoPlay from "./Pluggins/AutoPlay.js";
-import AutoPause from "./Pluggins/AutoPause.ts";
+import MediaPlayer from "./MediaPlayer";
+import AutoPlay from "./Pluggins/AutoPlay";
+import AutoPause from "./Pluggins/AutoPause";
 
 const video = document.querySelector("video");
 
@@ -9,10 +9,10 @@ const player = new MediaPlayer({
   plugins: [new AutoPlay(), new AutoPause()],
 });
 
-const playButton = document.querySelector("#playButton");
+const playButton: HTMLElement = document.querySelector("#playButton");
 playButton.onclick = () => player.togglePlay();
 
-const muteButton = document.querySelector("#muteButton");
+const muteButton: HTMLElement = document.querySelector("#muteButton");
 muteButton.onclick = () => {
   if (player.media.muted) {
     player.unmute();
